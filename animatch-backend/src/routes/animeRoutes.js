e4e2d81261b714edd1fac531407480b85f5403b1
jmensah365 <jmensah365@gmail.com>
2025-07-10 13:00:34 -0400

    cleaning up and commenting backend routes

--- animatch-backend/src/routes/animeRoutes.js
+++ animatch-backend/src/routes/animeRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { fetchAnimeListByAgeRating, fetchAnimeListByCategory, fetchAnimeListByName, fetchAnimeListBySeasonYear, fetchAnimeListByStatus, fetchAnimeListBySeason, addAnimeMetadata } from '../controllers/animeController.js';
+import { fetchAnimeListByAgeRating, fetchAnimeListByCategory, fetchAnimeListByName, fetchAnimeListBySeasonYear, fetchAnimeListByStatus, fetchAnimeListBySeason, addAnimeMetadata, getAnimeData } from '../controllers/animeController.js';
 
 //this file exposes the API endpoints
 const router = express.Router();
@@ -8,9 +8,9 @@ router.get("/category/:category", fetchAnimeListByCategory);
 router.get("/name/:name", fetchAnimeListByName);
 router.get("/ageRating/:ageRating", fetchAnimeListByAgeRating);
 router.get("/seasonYear/:year", fetchAnimeListBySeasonYear);
-//current, upcoming, or finished
-router.get("/status/:status", fetchAnimeListByStatus);
+router.get("/status/:status", fetchAnimeListByStatus); //types of statuses: current, upcoming, or finished
 router.get("/season/:season", fetchAnimeListBySeason);
 router.post("/insertMetadata", addAnimeMetadata );
+router.get("/getAnime", getAnimeData);
 
 export default router;
