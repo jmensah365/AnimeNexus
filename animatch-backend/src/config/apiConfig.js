8f58bd79e45811307e64847f83f88185ca5a040e
jmensah365 <jmensah365@gmail.com>
2025-09-07 14:02:41 -0400

    adding anilist api url

--- animatch-backend/src/config/apiConfig.js
+++ animatch-backend/src/config/apiConfig.js
@@ -1,3 +1,4 @@
 //This file holds the API keys and URLs for products used in the backend.
 export const KITSU_API_URL = process.env.KITSU_API_URL;
 export const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+export const ANILIST_API_URL = process.env.ANILIST_API_URL;
