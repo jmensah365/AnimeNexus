6abcd5ba73816fbf251892bfa6b014f381bec023
jmensah365 <jmensah365@gmail.com>
2025-09-07 13:58:58 -0400

    fixing about page margins and styling

--- animatch-frontend/src/components/HomeNavBar.jsx
+++ animatch-frontend/src/components/HomeNavBar.jsx
@@ -22,10 +22,10 @@ function HomeNavBar() {
                     <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
                         <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
                             <li>
-                                <a href="http://localhost:5173/" class="block py-2 px-3 text-black rounded-sm md:bg-transparent md:p-0 md:hover:bg-transparent md:hover:underline cursor-pointer" aria-current="page">Home</a>
+                                <a href="http://localhost:5173/" class="block py-2 px-3 text-black rounded-sm md:bg-transparent md:p-0 md:hover:bg-transparent md:hover:underline cursor-pointer hover:text-red-500" aria-current="page">Home</a>
                             </li>
                             <li>
-                                <a href="/about" class="block py-2 px-3 text-black rounded-sm md:hover:bg-transparen md:p-0 md:hover:underline cursor-pointer ">About</a>
+                                <a href="/about" class="block py-2 px-3 text-black rounded-sm md:hover:bg-transparen md:p-0 md:hover:underline cursor-pointer hover:text-red-500 ">About</a>
                             </li>                      
                         </ul>
                     </div>
--- animatch-frontend/src/pages/Home/About.jsx
+++ animatch-frontend/src/pages/Home/About.jsx
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import SpotlightCard from '../../components/Cards/SpotlightCard';
 import AniMatchTechStack from "../../components/AniMatchTechStack";
 import StyledButton from "../../components/Buttons/StyledButton";
+import HomeNavBar from "../../components/HomeNavBar";
 
 const About = () => {
     const containerVariants = {
@@ -24,101 +25,104 @@ const About = () => {
 
     return (
         <>
-            <div className="flex items-center justify-center bg-[url(/1354199.jpeg)] min-h-screen bg-center bg-cover">
-                <div className="max-w-4xl mx-auto p-6 text-white">
-                    <motion.div
-                        initial="hidden"
-                        animate="visible"
-                        variants={containerVariants}
-                        className="mb-12 text-center"
-                    >
-                        <motion.h1 variants={itemVariants} className="text-4xl font-bold ">
-                            Welcome to AniMatch!
-                        </motion.h1>
-                        <motion.p className="text-lg" variants={itemVariants}>
-                            Your personalized anime recommendation platform
-                        </motion.p>
-                    </motion.div>
-                    <motion.div
-                        initial="hidden"
-                        animate="visible"
-                        variants={containerVariants}
-                        className="grid grid-cols-1 md:grid-rows-4 gap-6"
-                    >
+            
+            <div className="bg-[url(/1354199.jpeg)] min-h-screen bg-center bg-cover">
+            <HomeNavBar />
+                <div className="flex items-center justify-center">
+                    <div className="max-w-4xl mx-auto p-6 text-white mt-30">
                         <motion.div
-                            variants={itemVariants}
+                            initial="hidden"
+                            animate="visible"
+                            variants={containerVariants}
+                            className="mb-12 text-center bg-black/50 rounded-2xl p-4"
                         >
-                            <SpotlightCard spotlightColor="rgba(64, 180, 100, 0.2)" className="h-full">
-                                <motion.h2
-                                    className="text-3xl font-bold mb-4 text-gray-400"
-                                    variants={itemVariants}>
-                                    About AniMatch
-                                </motion.h2>
-                                <motion.p
-                                    variants={itemVariants}
-                                >
-                                    AniMatch offers deep personalized recommendations. Driven by user data and input AniMatch aims to give more tailored recommendations to our users.
-                                    Using AI we are able to give nuanced recommendations, while also giving justification for its recommendations.
-                                </motion.p>
-                            </SpotlightCard>
+                            <motion.h1 variants={itemVariants} className="text-4xl font-bold ">
+                                Welcome to AniMatch!
+                            </motion.h1>
+                            <motion.p className="text-lg" variants={itemVariants}>
+                                Your personalized anime recommendation platform
+                            </motion.p>
                         </motion.div>
                         <motion.div
-                            variants={itemVariants}
+                            initial="hidden"
+                            animate="visible"
+                            variants={containerVariants}
+                            className="grid grid-cols-2 md:grid-rows-4 gap-6"
                         >
-                            <SpotlightCard className="h-full" spotlightColor="rgba(64, 180, 100, 0.2)">
-                                <motion.h2
-                                    className="text-3xl font-bold mb-4 text-gray-400"
-                                    variants={itemVariants}>
-                                    Our Mission
-                                </motion.h2>
-                                <motion.p
-                                    variants={itemVariants}
-                                >
-                                    AniMatch caters to both new and old anime watchers alike.
-                                    Whether you're a casual viewer trying to find where to start or a seasoned anime fan looking for new niche anime, AniMatch has something for everyone!
-                                </motion.p>
-                            </SpotlightCard>
-                        </motion.div>
-                        <motion.div
-                            variants={itemVariants}
-                        >
-                            <SpotlightCard spotlightColor="rgba(180, 64, 100, 0.2)" className="h-full">
-                                <motion.h2
-                                    className="text-3xl font-bold mb-4 text-gray-400"
-                                    variants={itemVariants}>
-                                    What Powers AniMatch
-                                </motion.h2>
-                                <AniMatchTechStack />
-                            </SpotlightCard>
-                        </motion.div>
-                        <motion.div
-                            variants={itemVariants}
-                        >
-                            <SpotlightCard spotlightColor="rgba(180, 64, 100, 0.2)" className="h-full">
-                                <motion.h2
-                                    className="text-3xl font-bold mb-4 text-gray-400"
-                                    variants={itemVariants}>
-                                    Start The Adventure
-                                </motion.h2>
-                                <motion.p
-                                    variants={itemVariants}
-                                >
-                                    Start you adventure today and get started by click the button below!
-                                </motion.p>
-                                <motion.button
-                                    variants={itemVariants}
-                                    className="mt-4"
-                                >
-                                    <StyledButton
-                                        Text="Get Started"
+                            <motion.div
+                                variants={itemVariants}
+                            >
+                                <SpotlightCard spotlightColor="rgba(64, 180, 100, 0.2)" className="h-full">
+                                    <motion.h2
+                                        className="text-3xl font-bold mb-4 text-gray-400"
+                                        variants={itemVariants}>
+                                        About AniMatch
+                                    </motion.h2>
+                                    <motion.p
+                                        variants={itemVariants}
+                                    >
+                                        AniMatch offers deep personalized recommendations. Driven by user data and input AniMatch aims to give more tailored recommendations to our users.
+                                        Using AI we are able to give nuanced recommendations, while also giving justification for its recommendations.
+                                    </motion.p>
+                                </SpotlightCard>
+                            </motion.div>
+                            <motion.div
+                                variants={itemVariants}
+                            >
+                                <SpotlightCard className="h-full" spotlightColor="rgba(64, 180, 100, 0.2)">
+                                    <motion.h2
+                                        className="text-3xl font-bold mb-4 text-gray-400"
+                                        variants={itemVariants}>
+                                        Our Mission
+                                    </motion.h2>
+                                    <motion.p
+                                        variants={itemVariants}
+                                    >
+                                        AniMatch caters to both new and old anime watchers alike.
+                                        Whether you're a casual viewer trying to find where to start or a seasoned anime fan looking for new niche anime, AniMatch has something for everyone!
+                                    </motion.p>
+                                </SpotlightCard>
+                            </motion.div>
+                            <motion.div
+                                variants={itemVariants}
+                            >
+                                <SpotlightCard spotlightColor="rgba(180, 64, 100, 0.2)" className="h-full">
+                                    {/* <motion.h2
+                                        className="text-3xl font-bold mb-4 text-gray-400"
+                                        variants={itemVariants}>
+                                        What Powers AniMatch
+                                    </motion.h2> */}
+                                    <AniMatchTechStack />
+                                </SpotlightCard>
+                            </motion.div>
+                            <motion.div
+                                variants={itemVariants}
+                            >
+                                <SpotlightCard spotlightColor="rgba(180, 64, 100, 0.2)" className="h-full">
+                                    <motion.h2
+                                        className="text-3xl font-bold mb-4 text-gray-400"
+                                        variants={itemVariants}>
+                                        Start The Adventure
+                                    </motion.h2>
+                                    <motion.p
+                                        variants={itemVariants}
+                                    >
+                                        Start you adventure today and get started by click the button below!
+                                    </motion.p>
+                                    <motion.button
+                                        variants={itemVariants}
+                                        className="mt-4"
+                                    >
+                                        <StyledButton
+                                            Text="Get Started"
 
-                                    />
-                                </motion.button>
-                            </SpotlightCard>
+                                        />
+                                    </motion.button>
+                                </SpotlightCard>
+                            </motion.div>
                         </motion.div>
-                    </motion.div>
+                    </div>
                 </div>
-
             </div>
         </>
 
