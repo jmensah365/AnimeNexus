c638c3c98b0e08feeab24b711cb75fdd2fa21b08
jmensah365 <jmensah365@gmail.com>
2025-09-11 12:39:27 -0400

    starting reactions page

---
+++ animatch-frontend/src/pages/Reactions.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+
+function Reactions() {
+    return (
+        <div className='bg-black min-h-screen'>
+            <Sidebar/>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Reactions
---
+++ animatch-frontend/src/services/reactionService.js
@@ -0,0 +1,50 @@
+export const fetchReactionsWithInfo = async (token) => {
+    const response = await fetch(`${import.meta.env.VITE_LOCAL_URL}/user-reactions/with-titles`, {
+        method: 'GET',
+        headers: { 
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+    });
+    if (!response.ok) throw new Error(await response.text());
+    return response.json();
+}
+
+export const createReaction = async ({ anime_id, reaction }, token) => {
+    const response = await fetch(`${import.meta.env.VITE_LOCAL_URL}/user-reactions/`, {
+        method: 'POST',
+        headers: { 
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({ anime_id, reaction }),
+    });
+    if (!response.ok) throw new Error(await response.text());
+    return response.json();
+}
+
+export const updateReaction = async ({ anime_id, reaction }, token) => {
+    const response = await fetch(`${import.meta.env.VITE_LOCAL_URL}/user-reactions/${anime_id}`, {
+        method: 'PUT',
+        headers: { 
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({ reaction }),
+    });
+    if (!response.ok) throw new Error(await response.text());
+    return response.json();
+}
+
+export const deleteReaction = async ({ reaction_id }, token) => {
+    const response = await fetch(`${import.meta.env.VITE_LOCAL_URL}/user-reactions/${reaction_id}`, {
+        method: 'DELETE',
+        headers: { 
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+    });
+
+    if (!response.ok) throw new Error(await response.text());
+    return;
+}
