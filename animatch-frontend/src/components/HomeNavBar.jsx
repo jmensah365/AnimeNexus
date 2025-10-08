aec81b56c1a7eb1f1e78a847e2979ee73cee3b69
jmensah365 <jmensah365@gmail.com>
2025-10-08 15:19:20 -0400

    updating navbar to use more general routes

--- animatch-frontend/src/components/HomeNavBar.jsx
+++ animatch-frontend/src/components/HomeNavBar.jsx
@@ -6,7 +6,7 @@ function HomeNavBar() {
         <>
             <nav class="bg-transparent fixed w-full z-20 top-0 start-0 left-0">
                 <div class="max-w-screen flex flex-wrap items-center justify-between mx-auto p-4 px-8">
-                    <a href="http://localhost:5173/" class="flex items-center space-x-3 rtl:space-x-reverse">
+                    <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
                         <img src={'/AniMatchLogo2.png'} class="h-14" alt="OncoTrials Logo"/>
                             <span class="self-center text-2xl font-semibold whitespace-nowrap text-black">AniMatch</span>
                     </a>
@@ -22,7 +22,7 @@ function HomeNavBar() {
                     <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
                         <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
                             <li>
-                                <a href="http://localhost:5173/" class="block py-2 px-3 text-black rounded-sm md:bg-transparent md:p-0 md:hover:bg-transparent md:hover:underline cursor-pointer hover:text-red-500" aria-current="page">Home</a>
+                                <a href="/" class="block py-2 px-3 text-black rounded-sm md:bg-transparent md:p-0 md:hover:bg-transparent md:hover:underline cursor-pointer hover:text-red-500" aria-current="page">Home</a>
                             </li>
                             <li>
                                 <a href="/about" class="block py-2 px-3 text-black rounded-sm md:hover:bg-transparen md:p-0 md:hover:underline cursor-pointer hover:text-red-500 ">About</a>
