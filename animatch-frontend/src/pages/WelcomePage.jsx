789689c8b5d582366ee4052d128d3f77048ee538
jmensah365 <jmensah365@gmail.com>
2026-04-20 11:55:03 -0400

    reverting button height change

--- animatch-frontend/src/pages/WelcomePage.jsx
+++ animatch-frontend/src/pages/WelcomePage.jsx
@@ -47,7 +47,7 @@ function WelcomePage() {
           <p className='font-semibold'>Your adventure awaits...</p>
           <button
             onClick={() => handleClick()}
-            className={`relative border hover:border-red-600 duration-500 group cursor-pointer text-sky-50 overflow-hidden h-20 w-56 rounded-md bg-red-800 p-2 flex justify-center items-center font-extrabold mt-3 ${genAndInsertAIRecs.isPending ? 'animate-pulse' : 'animate-none'
+            className={`relative border hover:border-red-600 duration-500 group cursor-pointer text-sky-50 overflow-hidden h-14 w-56 rounded-md bg-red-800 p-2 flex justify-center items-center font-extrabold mt-3 ${genAndInsertAIRecs.isPending ? 'animate-pulse' : 'animate-none'
               }`}
           >
             <div className="absolute z-0 w-48 h-48 rounded-full group-hover:scale-150 transition-all duration-500 ease-in-out bg-red-900 delay-150 group-hover:delay-75"></div>
