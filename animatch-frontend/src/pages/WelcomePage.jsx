78e7e5b2ae33b0f7f5d7b8b33962ad59e6abfb2a
jmensah365 <jmensah365@gmail.com>
2026-04-20 11:37:47 -0400

    enforcing black background

--- animatch-frontend/src/pages/WelcomePage.jsx
+++ animatch-frontend/src/pages/WelcomePage.jsx
@@ -24,7 +24,7 @@ function WelcomePage() {
   return (
     <>
 
-      <div className="relative w-full h-screen overflow-scroll">
+      <div className="relative w-full h-screen bg-black">
         {/* Particles background */}
         <Particles
           particleColors={['#ffffff', '#ffffff']}
